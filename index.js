5219988e042d6ef3fd286f28d757e25cb8f4cf2b
SUMIT SINGH <44521361+mssg12@users.noreply.github.com>
2023-10-27 01:45:58 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,143 @@
-// require('dotenv').config()
+require('dotenv').config()
 const express =require('express');
 const path = require('path');
 const app =express();
 const bodyParser = require('body-parser')
-const router =require('./routes/routes')
-const mongoose = require('./config/mongoose')
+const Habit = require('../models/habitModel')
 
-// Setting Up view engine and Views Folder
-app.set("view engine","ejs");
-app.set('views', path.join(__dirname, 'views'));
+function getD(n) {
+    let d = new Date();
+    d.setDate(d.getDate() + n);
+    var newDate = d.toLocaleDateString('pt-br').split('/').reverse().join('-');
+    var day;
+    switch (d.getDay()) {
+        case 0: day = 'Sun';
+            break;
+        case 1: day = 'Mon';
+            break;
+        case 2: day = 'Tue';
+            break;
+        case 3: day = 'Wed';
+            break;
+        case 4: day = 'Thu';
+            break;
+        case 5: day = 'Fri';
+            break;
+        case 6: day = 'Sat';
+            break;
+    }
+    return { date: newDate, day };
+}
 
-// Connecting Assets Folder / Static Files
-app.use("/assets", express.static('./assets'));
-app.use( bodyParser.urlencoded({ extended: true }) );
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+module.exports.home = function (req, resp) {
+    
+    Habit.find().select('-updatedAt -createdAt -__v').sort({ _id: -1 })
+        .then(habits => {
+            var days = [];
+            days.push(getD(0));
+            days.push(getD(1));
+            days.push(getD(2));
+            days.push(getD(3));
+            days.push(getD(4));
+            days.push(getD(5));
+            days.push(getD(6));
+            resp.render('habit', { habit: habits, days });
+        })
+        .catch(err => {
+            console.log(err);
+        }); 
+    }
 
-// Setting up routes
-app.use(router);
-const PORT = process.env.PORT || 8000
-app.listen(PORT,()=>{
-    console.log(`Server running at port: ${PORT}`);
-})
+module.exports.createHabit = function async(req, resp) {
+    const { content } = req.body;
+    console.log(content)
+    Habit.findOne({ content:content }).then(habit => {
+        if (habit) {
+            // Update Existing Habit Status
+            let dates = habit.dates, tzoffset = (new Date()).getTimezoneOffset() * 60000;
+            var today = (new Date(Date.now() - tzoffset)).toISOString().slice(0, 10);
+            dates.find(function (item, index) {
+                if (item.date === today) {
+                    console.log("Habit Already inserted in Database")
+                    
+                    resp.redirect('back');
+                }
+                else {
+                    dates.push({ date: today, complete: 'none' });
+                    habit.dates = dates;
+                    habit.save()
+                        .then(habit => {
+                            console.log(habit);
+                            resp.redirect('back');
+                        })
+                        .catch(err => console.log(err));
+                }
+            });
+        }
+        else {
+            let dates = [], tzoffset = (new Date()).getTimezoneOffset() * 60000;
+            var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, 10);
+            dates.push({ date: localISOTime, complete: 'none' });
+            const newHabit = new Habit({
+               content,
+                dates
+            });
+
+           
+            newHabit
+                .save()
+                .then(habit => {
+                    console.log(habit);
+                    resp.redirect('back');
+                })
+                .catch(err => console.log(err));
+        }
+         
+    })
+  
+}
+
+module.exports.getHabit = function (req, resp){
+    var d = req.query.date;
+    var id = req.query.id;
+    Habit.findById(id)
+        .then(habit => {
+            let dates = habit.dates;
+            let found = false;
+            dates.find(function (item, index) {
+                if (item.date === d) {
+                    if (item.complete === 'yes') {
+                        item.complete = 'no';
+                    }
+                    else if (item.complete === 'no') {
+                        item.complete = 'none'
+                    }
+                    else if (item.complete === 'none') {
+                        item.complete = 'yes'
+                    }
+                    found = true;
+                }
+            })
+            if (!found) {
+                dates.push({ date: d, complete: 'yes' })
+            }
+            habit.dates = dates;
+            return habit.save();
+        })
+        .then(updatedHabit => {
+            console.log(updatedHabit);
+            resp.redirect('back');
+        })
+        .catch(err => {
+            console.log("Habit status not updated", err);
+            resp.status(500).send("Error updating habit status");
+        });
+}
+
+module.exports.deletehabit =async function  (req, resp){
+    const documentProduct = await Habit.findOneAndRemove({ _id: req.params.id });
+    if (!documentProduct) {
+        resp.status(500).json(err);
+    } resp.redirect('/')
+}
